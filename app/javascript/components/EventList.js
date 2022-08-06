6f97cd3d07dd96a96e3286c9be09bbdc2982afe4
Cyrille Gawlowski <gawlowski.cyrille@gmail.com>
2022-08-06 16:45:12 -0400

    Styling

--- app/javascript/components/EventList.js
+++ app/javascript/components/EventList.js
@@ -19,7 +19,7 @@ const EventList = ({ events }) => {
   };
 
   return (
-    <section>
+    <section className="eventList">
       <h2>Events</h2>
       <ul>{renderEvents(events)}</ul>
     </section>
